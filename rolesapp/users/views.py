2b11cb2558d443fab05ec56a7bc5d425f1323535
GabyDs <gabydsch2001@gmail.com>
2024-11-26 22:43:06 -0300

    Verificar autenticación de usuarios y mostrar lista dinámica
    - Implementada la lógica en la vista para manejar solicitudes POST del formulario de login.
      - Se verifica si el nombre de usuario y contraseña existen en la base de datos.
      - Si las credenciales son correctas, se redirige a la vista que muestra la lista de usuarios.
      - En caso contrario, se redirige al panel de login.
    - Creado un template dinámico que lista los usuarios registrados.
      - Se incluye el nombre de usuario y el rol en la lista.

--- rolesapp/users/views.py
+++ rolesapp/users/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from django.urls import reverse
+
+from .models import User
 
 def login_view(request):
     # Renderizar el template
     return render(request, "login.html")
 
 def list_users(request):
-    return HttpResponse("Vista de lista de usuarios")
+
+    users = User.objects
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if User.objects.filter(username=username, password=password).exists():
+            return render(request, "list_users.html", {'users': users})
+        else:
+            return HttpResponseRedirect(reverse("users:login_view"))
