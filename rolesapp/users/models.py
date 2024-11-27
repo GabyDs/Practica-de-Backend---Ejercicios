caf05a244a8fa1b64373993a76dd75a46826bd4b
GabyDs <gabydsch2001@gmail.com>
2024-11-26 21:29:47 -0300

    feat(auth): Modelo de Usuario creado

--- rolesapp/users/models.py
+++ rolesapp/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+    # 0 -> usuario normal
+    # 1 -> usuario admin
+    rol = models.BooleanField(default=0)
