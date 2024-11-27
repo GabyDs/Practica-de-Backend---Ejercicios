6d46c279ecf1e05bf093665a5ad6f6639a07b6d3
GabyDs <gabydsch2001@gmail.com>
2024-11-26 21:30:47 -0300

    feat(auth): Vistas de inicio de sesión y lista de usuarios añadidas

--- rolesapp/users/views.py
+++ rolesapp/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def login_view(request):
+    # Renderizar el template
+    return render(request, "login.html")
+
+def list_users(request):
+    return HttpResponse("Vista de lista de usuarios")
